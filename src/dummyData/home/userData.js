6c039717159b830bd336a3a51bfc5d9d76789ce5
chandninigam <51984693+chandninigam@users.noreply.github.com>
2022-10-05 19:07:48 +0530

    Added Header and Stories in Home Screen
    Added Header and stories bar in Home Screen

---
+++ src/dummyData/home/userData.js
@@ -0,0 +1,63 @@
+// Data which is display in home Screen (Stories)
+export const DummyData = [
+  {
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwcP9l-2gFETJaQ3DzsEXcvB--tjjY9sSgog&usqp=CAU",
+    displayName: "Wanda Klein",
+  },
+  {
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLe5PABjXc17cjIMOibECLM7ppDwMmiDg6Dw&usqp=CAU",
+    displayName: "Jessi Libby",
+  },
+  {
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVO8W87IBRDJf0UPVE084vrMQWlv2prA8EyA&usqp=CAU",
+    displayName: "Belly Green",
+  },
+  {
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNPY27vtW-XQV8JpGRv8GPNBiLnqEwaYakAw&usqp=CAU",
+    displayName: "Andre Flores",
+  },
+  {
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyETGaiF1Iu9a-MbEYmo1D6yEbmwe0dDLkoQ&usqp=CAU",
+    displayName: "aime Allen",
+  },
+  {
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4j5EsloB48DnxRWOYQKJxT01dGj6cVFEDPQ&usqp=CAU",
+    displayName: "Libby",
+  },
+  {
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKNcdGKFiuFZMRYyQVRu1cIhkjK17PXs5ChQ&usqp=CAU",
+    displayName: "Belly Forst",
+  },
+  {
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwcP9l-2gFETJaQ3DzsEXcvB--tjjY9sSgog&usqp=CAU",
+    displayName: "John Smith",
+  },
+  {
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6Pak5sOsw6Lr3YYVUTYhGaRfB69eu6yGt1A&usqp=CAU",
+    displayName: "Demo",
+  },
+  {
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5NAISSOMJrxna3Dy9Oyc0G-pnB_t3RUucug&usqp=CAU",
+    displayName: "Prison Petter",
+  },
+  {
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlU4OTkdgw6A89uNg81qarHeZuLj4tuvlIVw&usqp=CAU",
+    displayName: "James",
+  },
+  {
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyETGaiF1Iu9a-MbEYmo1D6yEbmwe0dDLkoQ&usqp=CAU",
+    displayName: "Petter Allen",
+  },
+];

53db52e1c62aefc5d1b7b47f87cf1fb67e4c75fa
chandninigam <51984693+chandninigam@users.noreply.github.com>
2022-10-09 15:52:46 +0530

    Added Single Post (#5)
    - Added Single Post with liked, comment,shared options
    - Added LikedBy ,liked Total Number
    - Added Total Comments on Post

--- src/dummyData/home/postData.js
+++ src/dummyData/home/postData.js
@@ -7,6 +7,8 @@ export const DummyPostData = [
     userName: "sadhguru",
     actualPost:
       "https://static.sadhguru.org/d/46272/1633506837-1633506836326.jpg",
+    recentLikedPersonImage:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80",
     likedby: "monashriyadav",
     likedNumber: "89,458",
     postThought:

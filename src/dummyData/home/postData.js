53500ca08bd0abd43bd4d207d90933b6ed0b3994
chandninigam <51984693+chandninigam@users.noreply.github.com>
2022-10-10 20:32:30 +0530

    Added Post Data (#7)

--- src/dummyData/home/postData.js
+++ src/dummyData/home/postData.js
@@ -16,4 +16,34 @@ export const DummyPostData = [
     commentsNumber: "192",
     postTime: "12 hours ago",
   },
+  {
+    userProfile:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLe5PABjXc17cjIMOibECLM7ppDwMmiDg6Dw&usqp=CAU",
+    userName: "thing_like_queen",
+    actualPost:
+      "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/quote-instagram-post-template-design-0366d8bac0883814ff16acf75f0250f7_screen.jpg?ts=1619797855",
+    recentLikedPersonImage:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80",
+    likedby: "neha_mourya",
+    likedNumber: "10,458",
+    postThought:
+      "Vijaydashami is about conquering the basic qualities if existence",
+    commentsNumber: "152",
+    postTime: "15 hours ago",
+  },
+  {
+    userProfile:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVO8W87IBRDJf0UPVE084vrMQWlv2prA8EyA&usqp=CAU",
+    userName: "Belly Green",
+    actualPost:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnFpYeHHsAMqhqG30rjvvjYG5WTCXDOqi7YS8v9bmDgT4pSr-gPcVeqI-fVp_hob0l9vk&usqp=CAU",
+    recentLikedPersonImage:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80",
+    likedby: "shivangi_sexena",
+    likedNumber: "18,458",
+    postThought:
+      "Vijaydashami is about conquering the basic qualities if existence",
+    commentsNumber: "152",
+    postTime: "16 hours ago",
+  },
 ];

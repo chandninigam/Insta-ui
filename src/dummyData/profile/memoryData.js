25355c0ca72e0b87d51492f4ea2756967cf5e3ff
chandninigam <51984693+chandninigam@users.noreply.github.com>
2022-10-14 20:53:14 +0530

    Added short Memories in Profile tab (#13)

---
+++ src/dummyData/profile/memoryData.js
@@ -0,0 +1,15 @@
+// Memories post
+export const MemoriesData = [
+  {
+    url: "https://i.pinimg.com/736x/de/d8/21/ded8219a20a08d4c4c5d4285d08f65a6.jpg",
+    tagName: "Memory",
+  },
+  {
+    url: "https://plus.unsplash.com/premium_photo-1664100194845-468e6495dc66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8ZnJpZW5kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    tagName: "friends",
+  },
+  {
+    url: "https://media.istockphoto.com/photos/big-family-picture-id1223008077?k=20&m=1223008077&s=612x612&w=0&h=DbUbC-qSUpb7_HWxaYfnaZ14RbYWMSkPQF__HVpSfiQ=",
+    tagName: "Family",
+  },
+];

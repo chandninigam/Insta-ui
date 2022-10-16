b408590af4e917d4397603618dc2cfa04d538b36
chandninigam <51984693+chandninigam@users.noreply.github.com>
2022-10-16 19:37:50 +0530

    Added Posts and TaggedPost
    Added Posts and TaggedPost in Profile Tab

---
+++ src/dummyData/profile/post.js
@@ -0,0 +1,49 @@
+//  User Actual Post Share on Instagarm
+
+export const PostsArray = [
+  {
+    url: "https://media.istockphoto.com/photos/beautiful-multi-generational-family-sitting-together-on-couch-at-home-picture-id1313461844?k=20&m=1313461844&s=612x612&w=0&h=eqpU3myiu5o5YhvaRGNJsqVPotvokPaWXcVINU6QdxY=",
+  },
+  {
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlpijPo7tRUz6qgcgC3FHFjQFgGFN4Qop_ow&usqp=CAU",
+  },
+  {
+    url: "https://media.istockphoto.com/photos/hand-overlap-picture-id674177296?k=20&m=674177296&s=612x612&w=0&h=DdbuVrZRqVIs3lkz5v5c-BL5m0054DgCapI6vfwyZOE=",
+  },
+  {
+    url: "https://media.istockphoto.com/photos/happiness-moment-of-family-at-the-beachstock-photo-picture-id1323538427?k=20&m=1323538427&s=612x612&w=0&h=YvDiTmLlfEHrr2ruXdFiN7IQntsu_pvQkqMnA4Qo178=",
+  },
+  {
+    url: "https://media.istockphoto.com/photos/christmas-dinner-picture-id578095894?k=20&m=578095894&s=612x612&w=0&h=CfdX51gdI-INYQmktoSIft6nDkx6wv0a5heu5XIj13Q=",
+  },
+  {
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb-P9oAIPXYruU6VlZsm7rUDztd2p7RrGK9w&usqp=CAU",
+  },
+  {
+    url: "https://media.istockphoto.com/photos/happy-family-celebrating-thanksgiving-day-picture-id1175644971?k=20&m=1175644971&s=612x612&w=0&h=QKNe5CxtXO-ar8VUZDKs6apethBZ3RmiuRW-EXf3fe4=",
+  },
+];
+
+export const TaggedPostArray = [
+  {
+    uri: "https://www.istockphoto.com/photo/multi-generation-family-on-beach-gm170536938-21060039",
+  },
+  {
+    uri: "https://media.istockphoto.com/photos/lovely-big-family-with-four-cheerful-sons-are-playing-on-yellow-in-picture-id1060002688?k=20&m=1060002688&s=612x612&w=0&h=2m06GtxFCNB94VOCKFgUu1Ebm7XK2C0MTF-HG5AXtiw=",
+  },
+  {
+    uri: "https://media.istockphoto.com/photos/family-reunion-picture-id478224572?k=20&m=478224572&s=612x612&w=0&h=nhEeNchnhwdKRKVPohDijLNLXgupPr7T6HgIslWWfP8=",
+  },
+  {
+    url: "https://media.istockphoto.com/photos/beautiful-multi-generational-family-sitting-together-on-couch-at-home-picture-id1313461844?k=20&m=1313461844&s=612x612&w=0&h=eqpU3myiu5o5YhvaRGNJsqVPotvokPaWXcVINU6QdxY=",
+  },
+  {
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlpijPo7tRUz6qgcgC3FHFjQFgGFN4Qop_ow&usqp=CAU",
+  },
+  {
+    url: "https://media.istockphoto.com/photos/hand-overlap-picture-id674177296?k=20&m=674177296&s=612x612&w=0&h=DdbuVrZRqVIs3lkz5v5c-BL5m0054DgCapI6vfwyZOE=",
+  },
+  {
+    url: "https://media.istockphoto.com/photos/happiness-moment-of-family-at-the-beachstock-photo-picture-id1323538427?k=20&m=1323538427&s=612x612&w=0&h=YvDiTmLlfEHrr2ruXdFiN7IQntsu_pvQkqMnA4Qo178=",
+  },
+];

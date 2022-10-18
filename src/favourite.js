dd7727e20fc1c8f5953bb3fe9685af74016175d3
chandninigam <51984693+chandninigam@users.noreply.github.com>
2022-10-18 21:53:30 +0530

    Added Next Tile Related to Current Post Shared by Friends

--- src/favourite.js
+++ src/favourite.js
@@ -9,10 +9,19 @@ export default function Favourite() {
       <Text style={styles.header}>Notifications</Text>
       {/* Pending Request */}
       <PendingRequest />
+      {/* New On Insta */}
+      <TileHeader title="New" />
+      <NewOnInsta />
     </View>
   );
 }
 
+// Tile Header
+function TileHeader({ title }) {
+  return <Text style={styles.tileHeader}>{title}</Text>;
+}
+
+// Pending Request Container
 function PendingRequest() {
   return (
     // Container
@@ -36,6 +45,40 @@ function PendingRequest() {
     </View>
   );
 }
+
+// New On Insta
+function NewOnInsta() {
+  return (
+    <View style={styles.newOnInstaContainer}>
+      <Image
+        source={{
+          uri: "https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=584",
+        }}
+        style={styles.onInstaImage}
+      />
+      <Image
+        source={{
+          uri: "https://media.nngroup.com/media/people/photos/2022-portrait-page-3.jpg.600x600_q75_autocrop_crop-smart_upscale.jpg",
+        }}
+        style={styles.otherOnInstaImage}
+      />
+      <View style={styles.postRelatedInfo}>
+        <Text style={styles.suggText}>Suggested:</Text>
+        <Text style={styles.postDetail}>
+          makeoverbyaanchalbhatia, hiteshchoudharyofficial and
+          urvashi_kiran_sharma
+        </Text>
+      </View>
+      <Image
+        source={{
+          uri: "https://www.pixinvent.com/materialize-material-design-admin-template/laravel/demo-4/images/avatar/avatar-7.png",
+        }}
+        style={styles.recentPost}
+      />
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
   mainContainer: {
     display: "flex",
@@ -80,5 +123,47 @@ const styles = StyleSheet.create({
     paddingTop: height / 250,
     fontSize: height / 64,
     color: "#403f3f",
+  },
+  tileHeader: {
+    paddingVertical: height / 48,
+    fontSize: height / 44,
+    fontWeight: "500",
+  },
+  newOnInstaContainer: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  onInstaImage: {
+    position: "relative",
+    height: height / 24,
+    width: height / 24,
+    borderRadius: height / 12,
+  },
+  otherOnInstaImage: {
+    position: "absolute",
+    height: height / 24,
+    width: height / 24,
+    borderRadius: height / 12,
+    borderColor: "white",
+    borderWidth: 1,
+    top: 8,
+    left: 16,
+  },
+  postRelatedInfo: {
+    display: "flex",
+    paddingHorizontal: height / 32,
+    flex: 1,
+  },
+  suggText: {
+    fontSize: height / 56,
+    color: "#5c5c5c",
+  },
+  postDetail: {
+    fontSize: height / 64,
+    fontWeight: "500",
+  },
+  recentPost: {
+    height: height / 20,
+    width: height / 20,
   },
 });

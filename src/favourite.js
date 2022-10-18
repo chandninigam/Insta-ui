acbdd1617a01a851acc251ed06d3703b38c171e7
chandninigam <51984693+chandninigam@users.noreply.github.com>
2022-10-18 20:19:25 +0530

    Added Header in Favourite Tab (#20)

--- src/favourite.js
+++ src/favourite.js
@@ -1,14 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+
+const { height } = Dimensions.get("window");
 
 export default function Favourite() {
   return (
     <View style={styles.mainContainer}>
-      <Text>Favourite Screen</Text>
+      <Text style={styles.header}>Notifications</Text>
     </View>
   );
 }
 const styles = StyleSheet.create({
   mainContainer: {
-    marginTop: 50,
+    marginTop: height / 20,
+    marginHorizontal: height / 80,
+  },
+  header: {
+    fontSize: height / 32,
+    fontWeight: "500",
   },
 });

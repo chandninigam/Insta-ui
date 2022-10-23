63d141f1721e627dc63407feb20c24c5da90c087
chandninigam <51984693+chandninigam@users.noreply.github.com>
2022-10-23 17:57:17 +0530

    Added Header in reel tab (#27)

--- src/reel.js
+++ src/reel.js
@@ -7,7 +7,7 @@ import {
   ScrollView,
   TouchableOpacity,
 } from "react-native";
-import { Feather as Icon } from "@expo/vector-icons";
+import { Feather as Icon, AntDesign as Icon2 } from "@expo/vector-icons";
 
 const { height, width } = Dimensions.get("window");
 
@@ -24,11 +24,15 @@ export default function Reel() {
         />
         {/* Container which is display on Image  */}
         <View style={styles.textContainer}>
+          {/* Header */}
           <View style={styles.header}>
             <Text style={styles.textHeading}>Reels</Text>
             <TouchableOpacity style={styles.camera}>
               <Icon name="camera" color="white" size={24} />
             </TouchableOpacity>
+          </View>
+          <View style={styles.favView}>
+            <Icon2 name="hearto" size={24} color="white" />
           </View>
         </View>
       </ScrollView>
@@ -47,20 +51,26 @@ const styles = StyleSheet.create({
   textContainer: {
     position: "absolute",
     paddingHorizontal: height / 80,
+    height: height,
+    width: width,
   },
   header: {
     display: "flex",
     flexDirection: "row",
     marginTop: height / 50,
-    zIndex: -1,
-    backgroundColor: "red",
   },
   textHeading: {
     fontSize: 24,
+    flex: 1,
     fontWeight: "600",
     color: "white",
   },
   camera: {
     display: "flex",
+    marginRight: 0,
+  },
+  favView: {
+    display: "flex",
+    alignItems: "flex-end",
   },
 });

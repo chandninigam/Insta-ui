e9769e39e21ccf371e704b06a0b86f10b58bbfb5
chandninigam <51984693+chandninigam@users.noreply.github.com>
2022-10-23 19:40:15 +0530

    Added Icons like share , like ,comment,etc in reel tab

--- src/reel.js
+++ src/reel.js
@@ -7,7 +7,13 @@ import {
   ScrollView,
   TouchableOpacity,
 } from "react-native";
-import { Feather as Icon, AntDesign as Icon2 } from "@expo/vector-icons";
+import {
+  Feather as Icon,
+  AntDesign as Icon2,
+  Fontisto as Icon3,
+  Ionicons as Icon4,
+  Entypo as Icon5,
+} from "@expo/vector-icons";
 
 const { height, width } = Dimensions.get("window");
 
@@ -28,11 +34,26 @@ export default function Reel() {
           <View style={styles.header}>
             <Text style={styles.textHeading}>Reels</Text>
             <TouchableOpacity style={styles.camera}>
-              <Icon name="camera" color="white" size={24} />
+              <Icon name="camera" color="white" size={32} />
             </TouchableOpacity>
           </View>
+          {/* favourite */}
           <View style={styles.favView}>
-            <Icon2 name="hearto" size={24} color="white" />
+            <Icon2 name="hearto" size={32} color="white" />
+            <Text style={styles.likeNum}>36.5K</Text>
+          </View>
+          {/* Comment */}
+          <View style={styles.commentView}>
+            <Icon3 name="comment" size={32} color="white" />
+            <Text style={styles.likeNum}>11.7K</Text>
+          </View>
+          {/* Share */}
+          <View style={styles.commentView}>
+            <Icon4 name="paper-plane-outline" size={32} color="white" />
+          </View>
+          {/* More Option */}
+          <View style={styles.commentView}>
+            <Icon5 name="dots-three-vertical" size={24} color="white" />
           </View>
         </View>
       </ScrollView>
@@ -72,5 +93,17 @@ const styles = StyleSheet.create({
   favView: {
     display: "flex",
     alignItems: "flex-end",
+    marginTop: height / 2.5,
+  },
+  likeNum: {
+    color: "white",
+    paddingTop: 8,
+    display: "flex",
+    justifySelf: "center",
+  },
+  commentView: {
+    display: "flex",
+    alignItems: "flex-end",
+    paddingTop: height / 30,
   },
 });

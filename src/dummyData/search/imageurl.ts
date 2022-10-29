9b73d84b5d68a71e646d784233d134a765281946
chandninigam <51984693+chandninigam@users.noreply.github.com>
2022-10-29 12:31:18 +0530

    Added Search Tab (#32)

---
+++ src/dummyData/search/imageurl.ts
@@ -0,0 +1,29 @@
+
+// Data which having Image URL for Search Tab
+
+export const SearchImageUrl = [
+    {imgurl:"https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg"},
+    {imgurl:"https://www.ivint.org/wp-content/uploads/2017/09/Family.jpg"},
+    {imgurl:"https://images.news18.com/ibnlive/uploads/2022/04/dance-4.jpg"},
+    {imgurl:"https://www.myglobalviewpoint.com/wp-content/uploads/2018/03/Best-Travel-Quotes-to-Fuel-Your-Wanderlust.jpg"},
+    {imgurl:"https://media.istockphoto.com/photos/create-life-balance-message-on-a-notepaper-with-stone-formation-on-picture-id1356112888?k=20&m=1356112888&s=612x612&w=0&h=B7O5DyPpz3bUPr9uMTcuMwvo8yHFxSMwY2Mu5n0QzvY="},
+    {imgurl:"https://www.thestatesman.com/wp-content/uploads/2022/05/42879d5cbe591c2771ff96215cf809bc-1.jpg"},
+    {imgurl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKNcdGKFiuFZMRYyQVRu1cIhkjK17PXs5ChQ&usqp=CAU"},
+    {imgurl:"https://media.istockphoto.com/photos/twilight-at-spirit-island-picture-id485371557?b=1&k=20&m=485371557&s=170667a&w=0&h=TX9gNnTsXxKa5phnHgcPOn3k75msrIN0g-9qlKaTsqY="},
+    {imgurl:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Nandini_Ghosal.jpg/340px-Nandini_Ghosal.jpg"},
+    {imgurl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5qEosIZaUswplTbD4yVHjp_ya8ka-73qnIw&usqp=CAU"},
+    {imgurl:"https://cdn.lifehack.org/wp-content/uploads/2016/03/100-Inspirational-Quotes-That-Will-Give-You-Strength-During-Hard-Times-1024x768.jpeg"},
+    {imgurl:"https://upload.wikimedia.org/wikipedia/commons/3/38/Two_dancers.jpg"},
+    {imgurl:"https://images.news18.com/ibnlive/uploads/2022/01/jupiter-like-planet-16423911184x3.png"},
+    {imgurl:"https://media.istockphoto.com/photos/happy-african-american-family-and-their-dog-enjoying-at-home-picture-id1389678530?b=1&k=20&m=1389678530&s=170667a&w=0&h=gsTFP58wuLjV-acMeShYnwBKw6omfNlZLfD6bEAPIQA="},
+    {imgurl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIXP16MdPs7d6eBcnaktVwl694ZollJzKb3ARVqUYK1TuzkRkWm0SnukEzzwC7OHnhgRA&usqp=CAU"},
+    {imgurl:"https://images.unsplash.com/photo-1546587348-d12660c30c50?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bmF0dXJhbHxlbnwwfHwwfHw%3D&w=1000&q=80"},
+    {imgurl:"https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2021/04/Dance-Cover.jpg?w=960&h=600&crop=1"},
+    {imgurl:"https://i.ytimg.com/vi/k6tx8EJ5g9E/maxresdefault.jpg"},
+    {imgurl:"https://images.pexels.com/photos/2681319/pexels-photo-2681319.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"},
+    {imgurl:"https://www.verywellmind.com/thmb/z-mHv6NUGXNkxuqMD1s2nRe3LRQ=/2122x1194/smart/filters:no_upscale()/family-parents-grandparents-Morsa-Images-Taxi-56a906ad3df78cf772a2ef29.jpg"},
+    {imgurl:"https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg"},
+     
+
+]
+
--- src/search.js
+++ src/search.js
@@ -1,14 +1,75 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+  TextInput,
+  Image,
+  ScrollView,
+} from "react-native";
+import { Feather as Icon } from "@expo/vector-icons";
+import { SearchImageUrl } from "./dummyData/search/imageurl";
+
+const { height } = Dimensions.get("window");
 
 export default function Search() {
   return (
     <View style={styles.mainContainer}>
-      <Text>Search Screen</Text>
+      {/* Search Container */}
+      <View style={styles.searchView}>
+        <Icon name="search" size={24} color="white" />
+        <TextInput
+          placeholder="Search"
+          style={styles.textInput}
+          placeholderTextColor="white"
+        />
+      </View>
+      {/* Automatic suggested */}
+      <ScrollView
+        vertical
+        showsVerticalScrollIndicator={false}
+        style={styles.scroll}
+      >
+        <View style={styles.storyAutoSuggContainer}>
+          {SearchImageUrl.map((eachImage) => (
+            <ImagesComponent eachImage={eachImage} />
+          ))}
+        </View>
+      </ScrollView>
     </View>
   );
 }
+
+function ImagesComponent({ eachImage }) {
+  return <Image style={styles.storyImage} source={{ uri: eachImage.imgurl }} />;
+}
+
 const styles = StyleSheet.create({
   mainContainer: {
     marginTop: 50,
+  },
+  searchView: {
+    display: "flex",
+    flexDirection: "row",
+    marginHorizontal: height / 80,
+    marginTop: height / 80,
+    backgroundColor: "#949490",
+    padding: height / 120,
+    borderRadius: height / 70,
+  },
+  textInput: {
+    paddingLeft: 8,
+    fontSize: height / 50,
+  },
+  scroll: {
+    marginTop: height / 70,
+  },
+  storyAutoSuggContainer: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  storyImage: {
+    height: height / 5,
+    width: height / 6.5,
   },
 });
